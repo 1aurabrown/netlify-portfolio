fd4b723adc5f127ac83c32d606cc4887740050bb
Laura Brown <laurabrown1113@gmail.com>
2019-08-05 17:38:16 +0200

    begin to set up graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,27 @@
  */
 
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
+  siteMetadata: {
+    title: "Laura Brown",
+  },
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/_data/pages/`,
+        ignore: [`**/\.*`],
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/_data/project/`,
+        ignore: [`**/\.*`],
+      }
+    },
+    `gatsby-transformer-yaml`,
+  ]
 }
